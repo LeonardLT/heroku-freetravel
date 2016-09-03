4ef2608cff5696ca9672ec675bb3bb693f7e4f7f
Leonard LiuTao <364125168@qq.com>
2016-09-03 16:20:36 +0800

    tmp

--- app/db/db.js
+++ app/db/db.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 
 module.exports = {
   connect: function (mode, callback) {
-    let url = 'mongodb://localhost/freetravel-db';
+    let url =process.env.PROD_MONGODB ||'mongodb://localhost/freetravel-db';
     if (mode === 'test') {
       url = 'mongodb://localhost/freetravel-test-db';
     }
     console.log('--db connect success');
     // mongoose.connect(url, callback);
-    mongoose.connect(process.env.PROD_MONGODB,callback);
+    mongoose.connect(url,callback);
   },
   close: function (callback) {
     mongoose.connection.close(callback);
